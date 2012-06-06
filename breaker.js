1ec7ab5ac35dc69188255b575c4822cadb9644fc
Matthew Bishop <matt.bis@gmail.com>
2012-06-06 22:08:10 +0100

    Initial commit and structure

---
+++ breaker.js
@@ -0,0 +1,27 @@
+﻿
+var breaker = (function(){
+	var 
+    c = $('#canvas')[0],
+    context = c.getContext('2d'),
+    
+    "dimensions" = {
+      "canvas" : { "w": 640, "h": 480 },
+      "brick" : { "w": 200, "h": 40 }, 
+      "bat": { "w": 100, "h": 25 }
+    },
+    
+    "positions" = {
+      "bat": { "x": , "y": }
+    },
+    
+    "deltas" = { 
+      "bat": { x: 0, y: 0 }
+    };
+	
+	return {
+		
+	};
+	
+})();
+
+$.ready(breaker);
