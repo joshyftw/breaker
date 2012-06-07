728be94e96b72d5687f0c91a156ddb3dec96410c
Matthew Bishop <matt.bis@gmail.com>
2012-06-07 22:45:04 +0100

    decided on a sensible structure...

--- breaker.js
+++ breaker.js
@@ -1,78 +1,10 @@
 ﻿
 // simple self contained breakout!
-(function(breaker){
+(function( b ){
+
   var
-    w = window,
-    
     op = Object.prototype,
-    toString = op.toString,
-  
-    ctx = $("CANVAS#breaker")[0],
-    context = ctx.getContext("2d"),
-    
-    // default configuration
-    conf = {
-      "rows": 4,
-      "bricks_per_row": 8,
-      
-      // TODO later this could be the css values populated, enabling the object to be self contained, without CSS
-      "padding": { // TODO or is a margin???
-        "bottom": 10 
-      },
-      
-      "dimensions": {
-        "canvas": {
-          "w": 640, 
-          "h": 480 
-        },
-        "brick": { 
-          "type":"rect", 
-          "w": "conf.dimensions.canvas.w / conf.bricks_per_row", 
-          "h": 40 
-        }
-        /*,
-        "bat": {
-          "type":"rect", 
-          "w": 100, 
-          "h": 25 
-        },
-        "ball": {
-          "type":"arc", 
-          "radius": 20
-        }
-        */
-      },
-      
-      "positions": {
-        "bat": { 
-          "x": "(conf.dimensions.canvas.w / 2) - (conf.dimensions.bat.w / 2)", 
-          "y": "(conf.dimensions.canvas.h - conf.dimensions.bat.h - conf.padding.bottom)"
-        },
-        "ball": { 
-          "x":0, 
-          "y":0 
-        }
-      }
-      
-    // end conf
-    },
-    
-    runtime = {
-      "deltas": { 
-        "bat": { 
-          "x": 0, 
-          "y": 0 
-        },
-        "ball": {
-          "x": 0,
-          "y": 0
-        }
-      },
-      
-      "score": 0
-      
-    // end runtime
-    };
+    toString = op.toString;
 	
   //---------------------------------------------------------------------------------------
   // METHODS:
@@ -80,7 +12,7 @@
   //  create one or many objects:
   //  params:
   //    id  String
-  breaker.draw = function(id){
+  b.draw = function( id ){
     var 
       d = (id) ? conf.dimensions[id] : conf.dimensions,
       cart = (id) ? conf.positions[id] : conf.positions;//esian
@@ -109,12 +41,27 @@
     }
   };
   
+  
+  
   //---------------------------------------------------------------------------------------
-  
   // STARTUP
-	draw();
-
-  $(breaker);
   
-// expose to window for external calls
-})(window.breaker = window.breaker || {});
+  b.__init__ = function( conf ){
+    b.conf = conf;
+    b.debug = conf.debug;
+  
+    if(b.debug) console.log("breaker:start");
+    
+    b.ctx = $("CANVAS#breaker")[0],
+    b.context = ctx.getContext("2d");
+      
+    b.draw();
+    
+    if(b.debug) console.log("breaker:end");
+  };
+  
+})(window.breaker = {});
+
+$.ready(function(){
+  b.init(breaker_default_conf);
+});
