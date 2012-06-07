dbdb664c3f85c89a2955b91741c7a44a1a5f670e
Matthew Bishop <matt.bis@gmail.com>
2012-06-07 22:27:31 +0100

    add config file

---
+++ breaker.conf.js
@@ -0,0 +1,60 @@
+var breaker_default_conf = {
+
+  "debug":true,
+
+  "rows": 4,
+  "bricks_per_row": 8,
+
+  // TODO later this could be the css values populated, enabling the object to be self contained, without CSS
+  "padding": { // TODO or is a margin???
+    "bottom": 10 
+  },
+
+  "dimensions": {
+    "canvas": {
+      "w": 640, 
+      "h": 480 
+    },
+    "brick": { 
+      "type":"rect", 
+      "w": "conf.dimensions.canvas.w / conf.bricks_per_row", 
+      "h": 40 
+    }
+    /*,
+    "bat": {
+      "type":"rect", 
+      "w": 100, 
+      "h": 25 
+    },
+    "ball": {
+      "type":"arc", 
+      "radius": 20
+    }
+    */
+  },
+  
+  "positions": {
+    "bat": { 
+      "x": "(conf.dimensions.canvas.w / 2) - (conf.dimensions.bat.w / 2)", 
+      "y": "(conf.dimensions.canvas.h - conf.dimensions.bat.h - conf.padding.bottom)"
+    },
+    "ball": { 
+      "x":0, 
+      "y":0 
+    }
+  }
+// end conf
+},
+
+runtime = {
+  "deltas": { 
+    "bat": { 
+      "x": 0, "y": 0 
+    },
+    "ball": {
+      "x": 0, "y": 0
+    }
+  },
+  "score": 0
+// end runtime
+};
