edad8ed30965193134a546fa2419a174b309cb4b
Matthew Bishop <matt.bis@gmail.com>
2013-02-15 19:45:52 +0000

    update

--- breaker.js
+++ breaker.js
@@ -1,170 +1,247 @@
 ﻿
-var l = {
-
-    makeArray: function(o){
-        var a=[];
-        for(var i in o) 
-        if(hasOwn.call(o,i)) 
-          a.push(o[i]);
-        return a;
-    }
-    
-};
-
 // BREAKER CLONE
 //  author: Matthew Bishop
 
-var breaker = (function(){
+var breaker = function(config){
+"use strict";
 
-    //private
-    var
-        op = Object.prototype,
-        toString = op.toString;
-
-    return {
-    
-        "runtime": {
-            
-            "entities": {
-                "bat": { "delta": { "x": 0, "y": 0 } },
-                "ball": { "delta": { "x": 0, "y": 0 } }
-            },
-            
-            "score": 0,
-            
-            "loops": 0
-            
-        // end runtime
-        },
-
-        //---------------------------------------------------------------------------------------
-
-        //  draw shapes
-        //  params:
-        //      id        String
-        //      type    String
-        draw: function( id, type ){
-            // allow this to create one or many shapes
-            var 
-                d = (id) ? this.conf.dimensions[id] : this.conf.dimensions,
-                cartesian = (id) ? this.conf.positions[id] : this.conf.positions;
+// PRIVATE
+var
+  op = Object.prototype,
+  toString = op.toString,
   
-            for(var i in d) {
-                var 
-                    o = d[i], 
-                    coords = cartesian[i];
-                
-                if( o && coords && o.type ) {
-                    if( type && o.type != type ) break; 
-                    
-                    // allow us to calculate positions at runtime, set to the functions result
-                    o.x = ( toString.call(coords.x) != "[object String]") ? (new Function("return " + coords.x + ";") ).apply(this, coords) : coords[i].x//,
-                    /*
-                    o.y = ( toString.call(coords.y) != "[object String]") ? (new Function("return " + coords.y + ";") ).apply(this, coords) : coords[i].y;
+  win = window, 
+  doc = win.document,
+  
+  type = function(o,s){
+		return 
+			(s === undefined) ? 
+				toString.call(o) : 
+				toString.call(o) === "[object "+s+"]";
+  },
+  
+  default_config = {
+    
+    "debug":              		false,
+    
+    "FPS":                		60,
+    "TICK_INTERVAL":      		1000,
+    
+    "rows":               		4,
+    "bricks_per_row":     		8,
+    
+    "dimensions": {
+      
+      "canvas":           		{ "w": 640, "h": 480 },
+      
+      "brick": { 
+        "type":           		"rect", 
+        "w":              		"Math.round(config.dimensions.canvas.w / config.bricks_per_row)", 
+        "h":              		40 
+      },
+      
+      "bat": { 
+        "type":           		"rect", 
+        "w":              		100, 
+        "h":              		25 
+      },
+      
+      "ball": {
+        "type":           		"arc", 
+        "radius":         		20
+      }
+      
+    }
+  },
+  
+  r = { // untime
+    
+    "container":         			doc.getElementById("container"),
+    "ectx":               		doc.getElementById("canvas"),
+    "ctx2d":          				r.ectx.getContext("2d"),
+      
+    // for legacy browsers
+    "oIntervalTimer":     		null,
+		"requestAnimationFrame":	null,
+    
+    "entities": {
+      "bat":              		{ "delta": { "x": 0, "y": 0 } },
+      "ball":             		{ "delta": { "x": 0, "y": 0 } }
+    },
+      
+    "score":              		0,
+    "loops":              		0,
+    
+    "positions": {
+			
+      "bat": { 
+        "x":              		"(config.dimensions.canvas.w / 2) - (config.dimensions.bat.w / 2)", // is there a bitwise optimisation here? probably
+        "y":              		"(config.dimensions.canvas.h - config.dimensions.bat.h)"
+      },
+			
+      "ball": { 
+        "x":              		0, 
+        "y":              		0
+      }
+			
+		}
+		
+  },
 
-                    if( this.debug ) console.log( toString.call(coords.x) );
-                    if( this.debug ) console.log( toString.call(coords.y) );
-                    if( this.debug ) console.log( o.x );
-                    if( this.debug ) console.log( o.y );
+	
+	// OLD:
+	
+  //---------------------------------------------------------------------------------------
 
-                    switch(o.type) {
-                        case "rect":
-                            this.context.drawRectangle( coords.x, coords.y, o.w, o.h );
-                        break;
-                        // simple for now ...
-                        case "arc":
-                            this.context.beginPath();
-                            this.context.arc(o.x, o.y, o.radius, 0, Math.PI*2, true);
-                            this.context.fill();
-                        break;
-                    }
-                    */
-                }
-            }
-        },
+  //  draw shapes
+  //  params:
+  //      id      String
+  //      type    String
+  draw = function( id, type ) {
+    // allow this to create one or many shapes
+    var 
+      d = (id) ? config.dimensions[id] : config.dimensions,
+      cartesian = (id) ? config.positions[id] : config.positions;
 
-        // update & draw
-        gameLoop: function() {
-            var l = this.entities.length;
-            for( var i = 0;i < l; i++ ) {
-                this.entities[i].updateDraw();
-            }
-        },
-
-        //---------------------------------------------------------------------------------------
-
-        // based on http://www.playmycode.com/blog/2011/08/building-a-game-mainloop-in-javascript/
-        run: function() {
-            var 
-                animFrame = 
-                    window.requestAnimationFrame ||
-                    window.webkitRequestAnimationFrame ||
-                    window.mozRequestAnimationFrame  ||
-                    window.oRequestAnimationFrame  ||
-                    window.msRequestAnimationFrame  ||
-                    null ;
-            
-            if( animFrame ) {
-                // use MozBeforePaint for performance enhancement in FireFox
-                if ( $.browser.mozilla ) {
-                    
-                    var recursiveAnimation = function() {
-                        breaker.gameLoop();
-                        animFrame();
-                    };
-                    window.addEventListener("MozBeforePaint", recursiveAnim, false);
-                    
-                } 
-                // other browsers
-                else {
-                    // webkit, allows the canvas object to be passed in enhance performance.
-                    var recursiveAnimation = function() {
-                        breaker.gameLoop();
-                        animFrame(recursiveAnimation, this.runtime.ctx);
-                    };
-                    
-                }
-            }
-            else if( animFrame === null ) {
-                var ONE_FRAME = this.conf.TICK_INTERVAL / this.conf.FPS;
-                setInterval( breaker.gameLoop, this.runtime.ONE_FRAME );
-                //clearInterval(this.oTimer);
-            }
-            
-            //this.runtime.requestId = window.requestAnimationFrame;
-            //window.cancelAnimationFrame(this.runtime.requestId);
-            //this.runtime.requestId = null;
-            
-            return function() {
-                
-            }
-            
-        },
+    for(var i in d) {
+      var 
+        o = d[i], 
+        coords = cartesian[i];
+      
+      if( o === undefined && coords && o.type ) {
+        if( type && o.type != type ) break; 
         
-        stop: function() {
-        },
+        o.x = (type(coords.x, "String")) ? 
+          (new Function("return " + coords.x + ";")).apply(this, coords) : 
+					(type(coords.x, "Number")) ? 
+						coords[i].x : 
+						null;
+				
+        o.y = (type(coords.y, "String")) ? 
+          (new Function("return " + coords.y + ";")).apply(this, coords) : 
+          (type(coords.y, "Number")) ? 
+						coords[i].y : 
+						null;
+				
+        if(config.debug) { 
+					console.log("coords.x type: "+type(coords.x));
+					console.log("coords.y type: "+type(coords.y));
+					console.log("o.x: "+o.x+"o.y: "+o.y);
+				}
         
-        main: function(conf) {
-            this.conf = conf;
-            this.debug = conf.debug;
+				// action each shape type
+        switch(o.type) {
+          case "rect":
+            r.ctx2d.drawRectangle(coords.x, coords.y, o.w, o.h);
+          break;
+          // simple for now ...
+          case "arc":
+            r.ctx2d.beginPath();
+            r.ctx2d.arc(o.x, o.y, o.radius, 0, Math.PI*2, true);
+            r.ctx2d.fill();
+					break;
+					// polygon!
+					//	c2.fillStyle = '#f00';
+					//	c2.beginPath();
+					//	c2.moveTo(0, 0);
+					//	c2.lineTo(100,50);
+					//	c2.lineTo(50, 100);
+					//	c2.lineTo(0, 90);
+					//	c2.closePath();
+					//	c2.fill();
+					case "polygon":
+						for(var i in o) {
+							r.
+						}
+					break;
+				}				
+      }
+    }
+  },
 
-            if(this.debug) console.log("breaker:start");
+  //---------------------------------------------------------------------------------------
 
-            this.ctx = $("DIV.breaker CANVAS")[0],
-            this.context = this.ctx.getContext("2d");
+  fGameLoopWorker = function() {
+    var i=r.entities.length-1; 
+    do r.entities[i].updateDraw(); while(i--);
+  },
 
-            this.draw();
-            //this.run();
-            
-            if(this.debug) console.log("breaker:end");
-        }
-        
-    };	
+  // based on http://www.playmycode.com/blog/2011/08/building-a-game-mainloop-in-javascript/
+  run = function() {
+		r.requestAnimationFrame = 
+			win.requestAnimationFrame ||
+			win.webkitRequestAnimationFrame ||
+			win.mozRequestAnimationFrame  ||
+			win.oRequestAnimationFrame  ||
+			win.msRequestAnimationFrame  ||
+			null;
+		var bLegacyBrowser = (fAnimFrame === null);		
+    
+    if( r.requestAnimationFrame !== null ) {
+      // use MozBeforePaint for performance enhancement in FireFox
+      if ( $.browser.moz ) {
+        var 
+          fRecursiveAnimation = function() {
+            breaker.fGameLoopWorker();
+						r.requestAnimationFrame(fRecursiveAnimation);
+          };
+        //win.addEventListener("MozBeforePaint", fRecursiveAnimation, false);
+        r.mozRequestId = r.requestAnimationFrame(); // START
+      } 
+      // other browsers
+      else if( $.browser.webkit ) {
+        // webkit, allows the canvas object to be passed in enhance performance.
+        var 
+          fRecursiveAnimation = function() {
+            breaker.fGameLoopWorker();
+            r.requestAnimationFrame(fRecursiveAnimation, r.ctx);
+          };
+        r.requestAnimationFrame(fRecursiveAnimation, r.ctx); // START
+      }
+    }
+    else if( bLegacyBrowser ) { // ie IE9
+      var 
+        iOneFrame = config.TICK_INTERVAL / config.FPS;
+      r.oIntervalTimer = setInterval( breaker.fGameLoopWorker, iOneFrame ); // START
+    }
+  },
+    
+  stop = function() {
+    if( r.requestFrameFrame !== null ) win.cancelAnimationFrame(r.mozRequestId); 
+    if( r.oIntervalTimer ) clearInterval(r.oIntervalTimer);
+  },
+    
+  init = function(conf) {
+    config = $.extend(default_config, conf);
 
-})();
+    if(config.debug) { 
+			console.log("breaker:start");
+			console.time();
+		}
+
+    this.draw();
+    //this.run();
+    
+    if(config.debug) { 
+			console.log("breaker:end");
+			console.timeEnd();
+		}
+  };
+	
+	// PUBLIC:
+	return {
+		"stop":							stop,
+		"init": 						init,
+		"fGameLoopWorker": 	fGameLoopWorker
+	};	
+};
 
 //---------------------------------------------------------------------------------------
 
-$(document).ready(function(){
-  breaker.main(breaker_default_conf);
+$(doc).ready(function(){ 
+  breaker.init({ 
+    "debug": true
+  });
 });
+
+})();
